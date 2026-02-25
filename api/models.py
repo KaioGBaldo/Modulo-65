d5259736575507e99d5931dd6bbc6e7496cf4f8a
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-02-24 23:43:21 -0300

    Add Category, Product, and Order models

--- api/models.py
+++ api/models.py
@@ -1,1 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
 
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ManyToManyField(Category, related_name="products")
+
+    def __str__(self):
+        return self.title
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Pedido {self.id} - {self.user.username}"
