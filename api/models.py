83133b4d537b653ee4a2d5626e7de0b8a8d5be07
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-03-28 23:27:04 -0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,24 +1,10 @@
-from django.db import models
-from django.contrib.auth.models import User
+from django.db import db
 
-class Category(models.Model):
-    title = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.title
-
-class Product(models.Model):
-    title = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ManyToManyField(Category, related_name="products")
+class Product(db.Model):
+    name = db.CharField(max_length=100)
+    description = db.TextField()
+    price = db.DecimalField(max_digits=10, decimal_places=2)
+    created_at = db.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
-
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ManyToManyField(Product)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Pedido {self.id} - {self.user.username}"
+        return self.name
