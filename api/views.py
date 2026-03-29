d585cb745b376991794c442625e61fecf2c337ef
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-03-28 23:28:24 -0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,25 +1,10 @@
-import asyncio
-import time
-from django.http import JsonResponse
+from rest_framework import viewsets
+from .models import Product
+from .serializers import ProductSerializer
 
-# Simulação de um contador ou tarefa pesada síncrona
-def contador_sincrono(request):
-    start_time = time.time()
-    time.sleep(2)  # Chamada bloqueante
-    duration = time.time() - start_time
-    return JsonResponse({
-        "status": "Síncrono finalizado (Bloqueante)",
-        "segundos_esperados": 2,
-        "duracao_real": round(duration, 2)
-    })
-
-# View Assíncrona correta
-async def contador_assincrono(request):
-    start_time = time.time()
-    await asyncio.sleep(2)  # Chamada não-bloqueante
-    duration = time.time() - start_time
-    return JsonResponse({
-        "status": "Assíncrono finalizado (Não-bloqueante)",
-        "segundos_esperados": 2,
-        "duracao_real": round(duration, 2)
-    })
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint que permite visualizar ou editar produtos.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
