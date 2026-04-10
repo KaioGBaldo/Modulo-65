0e580b6ab319114abc9d96641d1b4139638cc63c
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-04-09 22:27:33 -0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,10 +1,27 @@
 from django.db import db
 
+class Category(db.Model):
+    title = db.CharField(max_length=100)
+    slug = db.SlugField(unique=True)
+    description = db.TextField(blank=True, null=True)
+    active = db.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
 class Product(db.Model):
     name = db.CharField(max_length=100)
     description = db.TextField()
-    price = db.DecimalField(max_digits=10, decimal_places=2)
+    price = db.DecimalField(max_digits=10, decimal_length=2)
     created_at = db.DateTimeField(auto_now_add=True)
+    active = db.BooleanField(default=True)
+    # Relação com Categoria
+    category = db.ManyToManyField(Category, related_name='products', blank=True)
 
     def __str__(self):
         return self.name
+
+class Order(db.Model):
+    product = db.ManyToManyField(Product)
+    user = db.ForeignKey('auth.User', on_delete=db.CASCADE)
+    created_at = db.DateTimeField(auto_now_add=True)
