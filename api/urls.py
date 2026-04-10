6e3e4d1acd0c6369e24f36c613fce2dd9e4d243d
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-04-09 22:28:02 -0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
+from .views import ProductViewSet, CategoryViewSet, OrderViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
+router.register(r'category', CategoryViewSet)
+router.register(r'order', OrderViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
