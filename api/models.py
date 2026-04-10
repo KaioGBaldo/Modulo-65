47cb55e320f17f0902c88239f12a6175233da957
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-04-10 01:46:02 +0000

    3

--- api/models.py
+++ api/models.py
@@ -1,27 +1,26 @@
-from django.db import db
+from django.db import models  # <-- O erro estava aqui
 
-class Category(db.Model):
-    title = db.CharField(max_length=100)
-    slug = db.SlugField(unique=True)
-    description = db.TextField(blank=True, null=True)
-    active = db.BooleanField(default=True)
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True, null=True)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
 
-class Product(db.Model):
-    name = db.CharField(max_length=100)
-    description = db.TextField()
-    price = db.DecimalField(max_digits=10, decimal_length=2)
-    created_at = db.DateTimeField(auto_now_add=True)
-    active = db.BooleanField(default=True)
-    # Relação com Categoria
-    category = db.ManyToManyField(Category, related_name='products', blank=True)
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2) # Corrigido: decimal_places
+    created_at = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+    category = models.ManyToManyField(Category, related_name='products', blank=True)
 
     def __str__(self):
         return self.name
 
-class Order(db.Model):
-    product = db.ManyToManyField(Product)
-    user = db.ForeignKey('auth.User', on_delete=db.CASCADE)
-    created_at = db.DateTimeField(auto_now_add=True)
+class Order(models.Model):
+    product = models.ManyToManyField(Product)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
