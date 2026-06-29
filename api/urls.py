f48959f5f569605168932de4d8f5d7a4efa0bd4f
Kaio Grativol Baldo <78832356+KaioGBaldo@users.noreply.github.com>
2026-06-29 10:49:28 -0300

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, CategoryViewSet, OrderViewSet
+from .views import ProductViewSet, CategoryViewSet, OrderViewSet, async_counter_view
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -8,5 +8,6 @@ router.register(r'category', CategoryViewSet)
 router.register(r'order', OrderViewSet)
 
 urlpatterns = [
+    path('contador/', async_counter_view, name='async_contador'), # Rota exigida no último feedback
     path('', include(router.urls)),
 ]
